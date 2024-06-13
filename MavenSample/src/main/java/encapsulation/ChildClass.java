07ec8e42c25c5d3daac910c00380a4797c895cc2
USER <USER@192.168.1.48>
2024-06-13 10:49:08 +0530

    Encapsulation_June13

---
+++ MavenSample/src/main/java/encapsulation/ChildClass.java
@@ -0,0 +1,12 @@
+package encapsulation;
+
+public class ChildClass extends ParentClass {
+
+	public static void main(String[] args) {
+
+		ChildClass objChildClass = new ChildClass();
+		objChildClass.setName("abc");
+		objChildClass.getName();
+	}
+
+}
---
+++ MavenSample/src/main/java/encapsulation/ParentClass.java
@@ -0,0 +1,18 @@
+package encapsulation;
+
+public class ParentClass {
+
+	private String name;
+	
+	public void getName()
+	{
+		System.out.println(name);
+	}
+	
+	public void setName(String name)
+	{
+		this.name =name;
+	}
+	
+
+}
